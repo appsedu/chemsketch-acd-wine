be84ef6896bc2f78ef1b0513cd9e697a52f783c1
ailtonbsj <ailton.ifce@gmail.com>
2020-06-19 13:34:46 -0300

    fix shortcut

--- README.md
+++ README.md
@@ -5,5 +5,3 @@ Versão Gratuita do ACD/ChemSketch via Wine.
 Versão: 18.2.1
 
 Depends: wine-stable, wine32, p7zip
-
-[Baixe o pacote Debian aqui!](https://drive.google.com/open?id=1HUFAEHGG1vegDeKfsZrI9GEZhViRRK7C)
--- compile.sh
+++ compile.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if [ ! -f deb_package/tmp/patched.7z ]; then
 	wget "https://drive.google.com/uc?id=1biOG_ges2_uFbhCKaMU0X_r66PVNxExe&export=download" -O ChemSketch-wine.7z
+fi
+if [ -f ChemSketch-wine.7z ]; then
 	mv ChemSketch-wine.7z deb_package/tmp/patched.7z
 fi
 dpkg-deb -b deb_package .
